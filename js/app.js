11adefb322d7a6369c9d81b9efa70bd2208d9da4
Jhonathan O'Brian Saa Se <64106872+InterTech20@users.noreply.github.com>
2020-10-25 19:32:53 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,84 @@
+//Navegation Menu
+let bar_icon = document.querySelector('.bar-icon');
+let nav = document.querySelector('.nav');
+let enlaces = document.querySelector('.container-list');
+let inicio= document.querySelector('.inicio');
+let nosotros= document.querySelector('.nosotros');
+let servicios= document.querySelector('.servicios');
+let contacto= document.querySelector('.contacto');
+let contador = true;
+
+bar_icon.addEventListener('click', function(){
+   this.classList.toggle('active');
+   
+   if (contador == true) {
+     enlaces.style.width = '100%';
+     enlaces.style.overflow = 'hidden';
+     enlaces.style.transition = '0.5s';
+
+     nav.style.background = '#281769';
+     nav.style.transition = '0.5s';
+
+     contador = false;
+
+   }else{
+     contador = true;
+     enlaces.style.width = '0%';
+     enlaces.style.overflow = 'hidden';
+     enlaces.style.transition = '0.5s';
+
+     nav.style.background = 'transparent';
+     nav.style.transition = '0.5s';
+
+   }
+
+});
+
+inicio.addEventListener('click', function(){
+
+   contador = false;
+   enlaces.style.width = '0%';
+   enlaces.style.overflow = 'hidden';
+   enlaces.style.transition = '0.5s';
+   nav.style.background = 'transparent';
+   nav.style.transition = '0.5s';
+   contador = true;
+
+});
+
+nosotros.addEventListener('click', function(){
+
+  contador = false;
+  enlaces.style.width = '0%';
+  enlaces.style.overflow = 'hidden';
+  enlaces.style.transition = '0.5s';
+  nav.style.background = 'transparent';
+  nav.style.transition = '0.5s';
+  contador = true;
+
+});
+
+
+servicios.addEventListener('click', function(){
+
+  contador = false;
+  enlaces.style.width = '0%';
+  enlaces.style.overflow = 'hidden';
+  enlaces.style.transition = '0.5s';
+  nav.style.background = 'transparent';
+  nav.style.transition = '0.5s';
+  contador = true;
+
+});
+
+contacto.addEventListener('click', function(){
+
+  contador = false;
+  enlaces.style.width = '0%';
+  enlaces.style.overflow = 'hidden';
+  enlaces.style.transition = '0.5s';
+  nav.style.background = 'transparent';
+  nav.style.transition = '0.5s';
+  contador = true;
+
+});
